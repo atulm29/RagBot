169bdb458745e9b8bf137842fa24a94b21f2d6f1
atulm29 <atul.more@flipick.com>
2025-12-09 14:17:40 +0530

    Angular application and docker file setup

--- RAGServer.APP/src/environments/environment.prod.ts
+++ RAGServer.APP/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://ragserver-api-232938788602.asia-south1.run.app/api',
   gcpProjectId: 'amq-admin-api',
   gcpBucketName: 'amq-rag-docs',
   vertexAiRegion: 'us-central1'
