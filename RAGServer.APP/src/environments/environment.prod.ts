9347bcbacdb4dc3b2e57fe913ccad7fc0eaa2a71
atulm29 <atul.more@flipick.com>
2025-12-10 10:54:06 +0530

    angular json file changes

--- RAGServer.APP/src/environments/environment.prod.ts
+++ RAGServer.APP/src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ragserver-api-232938788602.asia-south1.run.app/api',
-  gcpProjectId: 'amq-admin-api',
-  gcpBucketName: 'amq-rag-docs',
-  vertexAiRegion: 'us-central1'
+  apiUrl: 'https://ragserver-api-232938788602.asia-south1.run.app/api'
 };
--- RAGServer.APP/src/environments/environment.ts
+++ RAGServer.APP/src/environments/environment.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api',
-  gcpProjectId: 'amq-admin-api',
-  gcpBucketName: 'amq-rag-docs',
-  vertexAiRegion: 'us-central1'
 };
